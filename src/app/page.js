3502022b0b6ddbb04ba03e19c9e6dc3b04726cc0
AbdullahMukadam <abdullahmukadam21@gmail.com>
2024-12-18 23:24:24 -0800

    some style fix

--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,9 @@ export default function Home() {
 
 
   return (
-    <div className="w-full min-h-screen bg-black">
+    <div className="w-full min-h-screen bg-black flex items-center justify-center flex-col">
       <h1 className="text-white">Go to Recipes</h1>
-      <Link href={"/recipes"} className="text-white">Recipes</Link>
+      <Link href={"/recipes"} className="text-white border-2 border-white p-2 rounded-lg mt-2">Recipes</Link>
     </div>
   );
 }
